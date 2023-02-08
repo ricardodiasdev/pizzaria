a99167579d591f3d719032e0ed4c1a229f192407
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-08 09:07:24 -0300

    Criaçao de type e detalhes do  usuário

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -3,10 +3,13 @@ import { DetailUserService } from "../../services/user/DetailUserService";
 
 class DetailUserController {
   async handle(req: Request, res: Response) {
-    
+    const user_id = req.user_id;
+
+    console.log("ID do User: ", user_id)
+
     const detailUserService = new DetailUserService();
 
-    const user = await detailUserService.execute();
+    const user = await detailUserService.execute(user_id);
 
     return res.json(user);
   }
