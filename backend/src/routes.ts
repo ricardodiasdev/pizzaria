e1d95b5c29cca1d3fa285525afd2e7ee1c64b7ee
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-09 11:51:30 -0300

    Adicionando itens ao pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -58,5 +59,7 @@ router.get(
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle)
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 export { router };
