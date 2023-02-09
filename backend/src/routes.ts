a3accd6b5e248798111e0197bb66458fcec1b2d1
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-09 12:07:41 -0300

    Retirando itens da mesa

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -58,8 +59,14 @@ router.get(
 
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 
-router.delete("/order", isAuthenticated, new RemoveOrderController().handle)
+router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 
-router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.post("/order/add", isAuthenticated, new AddItemController().handle);
+
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 
 export { router };
