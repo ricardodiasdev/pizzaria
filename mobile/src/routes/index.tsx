9c59d99b53072a85a3955a42a20368bc03e78bef
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-17 09:34:23 -0300

    Criando tela de login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
@@ -8,6 +8,21 @@ import AuthRoutes from "./auth.routes";
 const Routes = () => {
   const isAuthenticated = false;
   const loading = false;
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#1D1D2E",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size={60} color="#FFF" />
+      </View>
+    );
+  }
 
   return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 };
