ca23c2e239e9b414acd6784f1dd825538f17029c
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-17 18:56:51 -0300

    Tela de novo pedido

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,17 +1,67 @@
-import React, {useContext, useState} from "react";
-import { View, Text, Button } from "react-native";
-
-import { AuthContext } from "../../contexts/AuthContext";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 
 const Dashboard = () => {
-
-  const {signOut} = useContext(AuthContext)
   return (
-    <View>
-      <Text>Dashboard</Text>
-      <Button title="Sair do APP" onPress={signOut}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Novo pedido</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Número da mesa"
+        keyboardType="numeric"
+        placeholderTextColor="#F0F0F0"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Abrir mesa</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 15,
+    backgroundColor: "#1d1d2e",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#FFF",
+    marginBottom: 24,
+  },
+  input: {
+    width: "90%",
+    height: 60,
+    backgroundColor: "#101026",
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    textAlign: "center",
+    fontSize: 22,
+    color: "#FFF",
+  },
+  button: {
+    width: "90%",
+    height: 40,
+    backgroundColor: "#3FFFA3",
+    borderRadius: 4,
+    marginVertical: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#101026",
+    fontWeight: "bold",
+  },
+});
 
 export default Dashboard;
