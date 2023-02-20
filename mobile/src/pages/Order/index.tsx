04581910d44afc942c3d97f907b59c434b2a13c4
Ricardo Dias <ricardodias.dev@gmail.com>
2023-02-19 21:06:27 -0300

    Adicioando itens

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   TextInput,
   Modal,
-  FlatList
+  FlatList,
 } from "react-native";
 
 import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
@@ -17,7 +17,7 @@ import { api } from "../../services/api";
 
 import ModalPicker from "../components/ModalPicker";
 
-import ListItem from '../components/ListItem';
+import ListItem from "../components/ListItem";
 
 type RouteDetailParams = {
   Order: {
@@ -117,21 +117,36 @@ const Order = () => {
   };
 
   const handleAddItem = async () => {
-    console.log('Adicionou...')
-  }
+    const response = await api.post("/order/add", {
+      order_id: route.params?.order_id,
+      product_id: productSelected?.id,
+      amount: Number(amount),
+    });
+
+    let data = {
+      id: response.data.id,
+      product_id: productSelected?.id as string,
+      name: productSelected?.name as string,
+      amount: amount,
+    };
+
+    setItems((oldArray) => [...oldArray, data]);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Mesa {route.params.number}</Text>
-        <TouchableOpacity>
-          <Feather
-            name="trash-2"
-            size={28}
-            color="#FF3F4B"
-            onPress={handleCloseOrder}
-          />
-        </TouchableOpacity>
+          {items.length === 0 && (
+            <TouchableOpacity>
+              <Feather
+                name="trash-2"
+                size={28}
+                color="#FF3F4B"
+                onPress={handleCloseOrder}
+              />
+            </TouchableOpacity>
+          )}
       </View>
 
       {category.length !== 0 && (
@@ -176,10 +191,10 @@ const Order = () => {
       </View>
       <FlatList
         showsHorizontalScrollIndicator={false}
-        style={{flex: 1, marginTop: 24}}
+        style={{ flex: 1, marginTop: 24 }}
         data={items}
         keyExtractor={(item) => item.id}
-        renderItem = {({item}) => <ListItem data={item}/>}
+        renderItem={({ item }) => <ListItem data={item} />}
       />
       <Modal
         transparent={true}
@@ -190,7 +205,6 @@ const Order = () => {
           handleCloseModal={() => setModalCategoryVisible(false)}
           options={category}
           selectedItem={handleChangeCategory}
-          
         />
       </Modal>
       <Modal
--- mobile/src/pages/components/ListItem/index.tsx
+++ mobile/src/pages/components/ListItem/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
 
 interface ItemProps {
   data: {
@@ -11,16 +13,36 @@ interface ItemProps {
   };
 }
 
-const ListItem = ({data}: ItemProps) => {
+const ListItem = ({ data }: ItemProps) => {
   return (
     <View style={styles.container}>
-      <Text>Item da Lista</Text>
+      <Text style={styles.item}>
+        {data.amount} - {data.name}
+      </Text>
+      <TouchableOpacity>
+        <Feather name="trash-2" color="#FF3F4B" size={25} />
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: '#101026',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginBottom: 12,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 4,
+    borderWidth: 0.3,
+    borderColor: '#8A8A8A'
+  },
+  item: {
+    color: '#FFF'
+  },
 });
 
 export default ListItem;
